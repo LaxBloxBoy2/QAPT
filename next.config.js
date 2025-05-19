86472cd9c08c8ece15047e813c55795ce5329aec
QAPT User <user@example.com>
2025-05-19 20:43:44 +0200

    Fix build errors: update next.config.js and remove conflicting pages

--- next.config.js
+++ next.config.js
@@ -1,24 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   experimental: {
-    serverActions: true,
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'qapt-evelnvvme-anwars-projects-98548d87.vercel.app']
+    }
   },
-  // This is the key configuration for src directory
-  distDir: '.next',
-  // Tell Next.js where to find the pages
-  webpack(config) {
-    return config;
-  },
+  distDir: '.next'
 };
 
-// This is the most important part - it tells Next.js to use the src directory
-const withSrcDirectory = (nextConfig) => {
-  return {
-    ...nextConfig,
-    srcDir: 'src',
-  };
-};
-
-module.exports = withSrcDirectory(nextConfig);
+module.exports = nextConfig;
